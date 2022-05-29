d001301bc1a15b273e8035e91a42e14790f7556f
Annyeong1 <79385636+Annyeong-One@users.noreply.github.com>
2022-05-29 14:12:57 +0900

    version 1.0.1
    new function: gcd, lcm
    ll is now obsolete, int redirect to long long directly

--- cppheader.cpp
+++ cppheader.cpp
@@ -3,5 +3,9 @@ using namespace std;
 #define f2(i,x) for((i)=0; (i)<(x); (i)++) // 2-var for
 #define f2b(i,x) for((i)=(x)-1; (i)>=0; (i)--) // 2-var backwards for
 #define f3(i,f,t) for((i)=(f);(i)<=(t);(i)++) // 3-var for
-#define ll long long int
-#define int ll
+#define int long long int
+int gcd(int a, int b){
+    if(a<b){a=a^b;b=a^b;a=a^b;}
+    return b==0?a:gcd(b,a%b);
+}
+int lcm(int a, int b){return a/gcd(a,b)*b;}
