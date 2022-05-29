e421b03af467ee8a7d36441a45151e792618e8b8
Annyeong1 <79385636+Annyeong-One@users.noreply.github.com>
2022-05-29 14:10:06 +0900

    Initial commit
    uses bits/stdc++.h
    uses std namespace
    macro: f2, f2b, f3, ll
    redirect: int to long long

---
+++ cppheader.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define f2(i,x) for((i)=0; (i)<(x); (i)++) // 2-var for
+#define f2b(i,x) for((i)=(x)-1; (i)>=0; (i)--) // 2-var backwards for
+#define f3(i,f,t) for((i)=(f);(i)<=(t);(i)++) // 3-var for
+#define ll long long int
+#define int ll
