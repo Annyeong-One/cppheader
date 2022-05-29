f6bab97554551204b4461d87ca0bb9395f81179d
Annyeong1 <79385636+Annyeong-One@users.noreply.github.com>
2022-05-29 18:50:23 +0900

    v1.0.2
    added endl redirect
    added fall(x)

--- cppheader.cpp
+++ cppheader.cpp
@@ -1,9 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define int long long int
 #define f2(i,x) for((i)=0; (i)<(x); (i)++) // 2-var for
 #define f2b(i,x) for((i)=(x)-1; (i)>=0; (i)--) // 2-var backwards for
 #define f3(i,f,t) for((i)=(f);(i)<=(t);(i)++) // 3-var for
-#define int long long int
+#define endl '\n'
+#define fall(x) (x).begin(), (x).end()
 int gcd(int a, int b){
     if(a<b){a=a^b;b=a^b;a=a^b;}
     return b==0?a:gcd(b,a%b);
