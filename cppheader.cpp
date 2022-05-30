e915c78024a3a6b5d1afd43a2ecfbf2c2be5fbcc
Annyeong1 <yoon0421airbus@gmail.com>
2022-05-30 13:43:50 +0900

    v1.1
    added types : redirect, macro, functon
    added 'signed main' declaration
    added redirect : endl to '\n'
    added macro: sort1, rev
    main() has cin/cout optimizations

--- cppheader.cpp
+++ cppheader.cpp
@@ -1,13 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define int long long int
-#define f2(i,x) for((i)=0; (i)<(x); (i)++) // 2-var for
-#define f2b(i,x) for((i)=(x)-1; (i)>=0; (i)--) // 2-var backwards for
-#define f3(i,f,t) for((i)=(f);(i)<=(t);(i)++) // 3-var for
-#define endl '\n'
-#define fall(x) (x).begin(), (x).end()
-int gcd(int a, int b){
-    if(a<b){a=a^b;b=a^b;a=a^b;}
-    return b==0?a:gcd(b,a%b);
+#define int long long int // redirect
+#define ll long long int // redirect
+#define endl '\n' // redirect
+#define fall(x) (x).begin(), (x).end() // macro
+#define f2(i,x) for((i)=0; (i)<(x); (i)++) // macro
+#define f2b(i,x) for((i)=(x)-1; (i)>=0; (i)--) // macro
+#define f3(i,f,t) for((i)=(f);(i)<=(t);(i)++) // macro
+#define sort1(x) sort(fall(x)) // macro
+#define rev(x) reverse(fall(x)) // macro
+int gcd(int a, int b){if(a<b){a=a^b;b=a^b;a=a^b;} return b==0?a:gcd(b,a%b);} // function
+int lcm(int a, int b){return a/gcd(a,b)*b;} // function
+// declarations
+
+// functions
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    // code
+
+    return 0;
 }
-int lcm(int a, int b){return a/gcd(a,b)*b;}
