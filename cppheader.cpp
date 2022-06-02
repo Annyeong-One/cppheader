bb11ce5cc7b79d36b7aca4d6023de1b372e851a7
Annyeong1 <yoon0421airbus@gmail.com>
2022-06-02 20:24:30 +0900

    v1.2
    New: SIMD header added

--- cppheader.cpp
+++ cppheader.cpp
@@ -1,7 +1,12 @@
 // https://github.com/Annyeong-One/cppheader
 // Annyeong1's header
-#include<bits/stdc++.h>
-using namespace std;
+#include<bits/stdc++.h> // basic all in one header
+#include <immintrin.h> // SIMD optimization
+using namespace std; // std functions
+#pragma GCC optimize("O3") // SIMD
+#pragma GCC optimize("Ofast") // SIMD
+#pragma GCC optimize("unroll-loops") // SIMD
+#pragma GCC target("avx,avx2,fma") // SIMD
 #define i32 signed // redirect
 #define int long long // redirect
 #define ll long long // redirect
@@ -12,6 +17,7 @@ using namespace std;
 #define f3(i,f,t) for((i)=(f);(i)<=(t);(i)++) // macro
 #define sort1(x) sort(fall(x)) // macro
 #define rev(x) reverse(fall(x)) // macro
+#define SIMDE_ENABLE_NATIVE_ALIASES
 int gcd(int a, int b){if(a<b){a=a^b;b=a^b;a=a^b;} return b==0?a:gcd(b,a%b);} // function
 int lcm(int a, int b){return a/gcd(a,b)*b;} // function
 // variable declarations
