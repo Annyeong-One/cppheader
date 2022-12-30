aac7f6740817b7b285723cc0adfc98e4fd7854cd
Annyeong1 <79385636+Annyeong-One@users.noreply.github.com>
2022-12-30 23:20:01 +0900

    Update cppheader.cpp

--- cppheader.cpp
+++ cppheader.cpp
@@ -1,10 +1,15 @@
 #include<bits/stdc++.h> // header
+#pragma GCC optimize("O3") // optimization
+#pragma GCC optimize("Ofast") // optimization
+#pragma GCC optimize("unroll-loops") // optimization
 using namespace std; // std function
 #define i32 signed // redirect
 #define int long long // redirect
 #define ll long long // redirect
 #define endl '\n' // redirect
 #define elif else if // redirect
+#define fi first // redirect
+#define se second // redirect
 #define f2(i,x) for((i)=0; (i)<(x); (i)++) // macro
 #define f2b(i,x) for((i)=(x)-1; (i)>=0; (i)--) // macro
 #define f3(i,f,t) for((i)=(f);(i)<=(t);(i)++) // macro
@@ -14,7 +19,7 @@ using namespace std; // std function
 #define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0) // macro
 int gcd(int a, int b){if(a<b){a=a^b;b=a^b;a=a^b;} return b==0?a:gcd(b,a%b);} // function
 int lcm(int a, int b){return a/gcd(a,b)*b;} // function
-int exp(int a, int b){int r=1;while(b){if(b&1)r=r*a;a=a*a;b>>=1;}return r;} // function
+int exp_mod(int a, int b, int m){int r=1;while(b){if(b&1)r=r*a%m;a=a*a%m;b>>=1;}return r%m;} // function
 // variable declarations
 
 // functions
